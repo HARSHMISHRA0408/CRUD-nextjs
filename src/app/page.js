4cd5c0ab74b91f9966e45d3d7836f06a77e88989
HARSHMISHRA0408 <harshmishra9713@gmail.com>
2024-07-21 23:35:22 +0530

    setup

--- src/app/page.js
+++ src/app/page.js
@@ -87,7 +87,9 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            Explore stard;jafjsldsjfoifjofjdlkfewofj lorem
+            lorem
+            lotea ter templates for Next.js.
           </p>
         </a>
 
